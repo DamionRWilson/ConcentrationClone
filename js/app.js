1ced8d79441f6bb2d7baf163586d51d2ba5c9cce
DamionRWilson <43320073+DamionRWilson@users.noreply.github.com>
2018-10-17 15:43:16 -0500

    Update app.js
    Clarify and clean up attempt #1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//cards are nice, let's make it possible to create them
 const cards = 	['fa-diamond', 'fa-diamond',
 				'fa-paper-plane-o', 'fa-paper-plane-o',
 				'fa-anchor', 'fa-anchor',
@@ -10,12 +11,11 @@ const cards = 	['fa-diamond', 'fa-diamond',
 
 function cardCreation(card) {
 	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
-};
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -23,10 +23,10 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+//variables for later use
 let flipped = [];
 let move = 0;
 let time = 0;
@@ -38,18 +38,21 @@ const restart = document.querySelector('.restart');
 const dupeMe = document.querySelector('.clone');
 let theCards = 0;
 let matched = 0;
-
 let timea = 0;
 let timeb = 0;
 let myTime = 0;
 
+//start the game
  function initiate() {
+ 	//stops any previously running timers
  	myTimeStop();
+ 	//create those cards mentioned before and fill the deck
  	const deck = document.querySelector('.deck');
  	let HTMLCards = shuffle(cards).map(function(card){
  		return cardCreation(card);
  	});
  	deck.innerHTML = HTMLCards.join('');
+ 	//reset variables
  	move = 0;
  	time = 0;
  	flipped = [];
@@ -58,6 +61,7 @@ let myTime = 0;
  	timeb = 0;
  	document.getElementById('num').innerText = move;
  	document.getElementById('timer').innerText = time;
+ 	//reset stars
  	if (starnum < 3) {
  		let dupeOne = dupeMe.cloneNode(true);
  		stars.appendChild(dupeOne);
@@ -68,9 +72,11 @@ let myTime = 0;
  		stars.appendChild(dupeTwo);
  		starnum++;
  	};
+ 	//Setup flip; i.e. the whole game
  	theCards = document.querySelectorAll('.card');
  	theCards.forEach(function(card) {
 		card.addEventListener('click', function(event){
+			//start timer
 			if (timea === 0){
 				timea = new Date().getTime();
 			};
@@ -80,11 +86,13 @@ let myTime = 0;
 					let delta = timeb - timea;
 					time = Math.floor(delta/1000);
 					document.getElementById('timer').innerText = time;
-				}, 250);
+				}, 250)
 			};
+			//flip card if not already flipped
 			if  (!card.classList.contains('match') && !card.classList.contains('open')){
 				card.classList.add('open', 'show');
 				flipped.push(card);
+				//check for match
 				if (flipped.length == 2){
 					move++;
 					document.getElementById('num').innerText = move;
@@ -93,6 +101,7 @@ let myTime = 0;
 							card.classList.remove('open', 'show');
 							card.classList.add('match');
 							matched++;
+							//check if you won
 							setTimeout(function win () {
 								if (matched == 16) {
 									window.alert(`Congratulations, you won!\nStar Rating: ${starnum}\nYou played for ${time} seconds\nClick "Okay" to play again!`)
@@ -100,14 +109,15 @@ let myTime = 0;
 								};
 							 }, 1);
 							flipped = [];
-						});
+						})
 					} else {
 					setTimeout(function( ){
 						flipped.forEach(function(card){
 							card.classList.remove('open', 'show');
 							flipped = [];
-						});
+						})
 					}, 500)};
+					//manage star rating
 					if (move < good) {
 						return;
 					} else if (move >= good && move < okay) {
@@ -131,6 +141,6 @@ let myTime = 0;
 
 function myTimeStop() {
 	clearInterval(myTime);
-};
+}
 
  initiate();
